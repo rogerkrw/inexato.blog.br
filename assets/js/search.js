41ff8a231e2693b8eab4c3a13e18488af4889dbc
rogerkrw <rogerkrw@users.noreply.github.com>
2021-04-28 13:43:52 +0000

    deploy: 55b81778485ae0cdd4bc483ec9190bf42c123c38

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/inexato.blog.br/assets/js/search-data.json', true);
+    request.open('GET', '/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
